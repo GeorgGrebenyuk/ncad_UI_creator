1179b02efd897e8378e32312d200e074094f79c5
Georg Grebenyuk <55656376+GeorgGrebenyuk@users.noreply.github.com>
2024-09-16 20:19:49 +0300

    adding app-project

---
+++ src/NC_UI_Creator_App/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NC_UI_Creator_App
+{
+    
+    public class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
